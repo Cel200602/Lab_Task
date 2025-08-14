237605dbf4ad15bc912e1bec23ab61b09f41dfa9
Cel200602 <5celine9c@gmail.com>
2025-08-14 14:24:00 +0530

    Update and rename script.js to Day5script.js

--- Day5script.js
+++ Day5script.js
@@ -1,31 +1,3 @@
-/*const arr=[1,2,3,4,5,6,7,8,9,10]
-//forEach
-arr.forEach((x)=>{console.log(x)})
-
-//filter
-let filt=arr.filter((x)=>x%2==0)
-console.log(filt)
-
-//reduce
-let sum=arr.reduce((acc,val)=>acc+val)
-console.log(sum)
-
-//slice
-let sl=arr.slice(3,8)           
-console.log(spl)
-
-//splice
-let spl=arr.splice(3,8)
-console.log(spl)
-console.log(arr)
-
-function rev(word){
-    [...word].reverse().join("")
-}
-let wd="JavaScript is Fun"
-let array=wd.split(' ')
-for(let i=0;i<array.length;i++)
-console.log(array)*/
 
 function analyze(){
 
@@ -56,4 +28,5 @@ for(let x in freq){
     console.log(x+" : "+freq[x])
 }
 }
+
 
