ad49362ec9d65fdb725b392b6c6f94086a19ea0b
Cel200602 <5celine9c@gmail.com>
2025-10-07 20:58:13 -0800

    Prg9

--- JS/script.js
+++ JS/script.js
@@ -1,28 +1,19 @@
-let secs=0;
-let mins=0;
-let interval;
-
-function start(){
-    if(!interval){
-   interval= setInterval(function(){
-    ++secs;
-    if(secs==60){
-        ++mins;
-        secs=0;
-    }
-    if(mins==60){
-        mins=0;
-    }
-    document.querySelector("#timer").innerHTML=`${mins}`.padStart(2,"0")+":"+`${secs}`.padStart(2,"0");
-},1000)
-    }
-    
+const image=document.querySelector(".carousel img")
+const arrimg=["https://blog.dookinternational.com/images/post-media/GleKq1736932680.jpg","https://blog.dookinternational.com/images/post-media/GleKq1736932680.jpg","https://i.natgeofe.com/k/54b5e731-f8d6-4a93-92e0-b7b137cac411/france-champs-elysees-paris.jpg","https://travelfrancebucketlist.com/wp-content/uploads/2019/05/Carcassonne-France-1024x683.jpg","https://travelfrancebucketlist.com/wp-content/uploads/2019/05/Carcassonne-France-1024x683.jpg","https://www.orfonline.org/public/uploads/posts/image/65a5414a8a121.png"]
+let imgind=0
+function next(){
+    if(arrimg.length-1>imgind)
+    imgind++;
+    else
+    imgind=0;
+    image.src=arrimg[imgind]
 }
 
-function stop(){
-    
+function prev(){
+    if(0<imgind)
+        imgind--;
+    else
+    imgind=arrimg.length-1
+    image.src=arrimg[imgind]
 }
-
-function reset(){
-    
-}
+document.querySelector(".btn button").addEventListener("click",prev)
